a26df10872c2b837ec5b653a25b081eda75f8566
Adarsh Srivastava <adarshs@iitk.ac.in>
2019-06-13 07:11:59 +0530

    Initial Repo Created

---
+++ app/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <h1> Here we'll put the Form! </h1>
+    <HelloWorld msg="Hey" />
+    <button @click="logout"> Logout </button>
+</div>
+</template>
+
+<script>
+import firebase from 'firebase';
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    logout: function() {
+      firebase.auth().signOut().then(() => {
+        this.$router.replace('login')
+      })
+    },
+
+  }
+}
+</script>
